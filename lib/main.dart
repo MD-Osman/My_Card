9b7f81eb78c232bbcc50fe4dbe6911f1cc9a17ea
Muhammed OSMAN <mohamedalothmani2001@gmail.com>
2024-05-23 11:21:07 +0300

    2 commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart' as launcher;
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
